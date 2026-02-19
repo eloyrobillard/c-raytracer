839109620462c292f5f1281f992b3f5772763eff
Eloy Robillard <eloy.robillard@gmail.com>
2026-02-19 10:37:27 +0100

    Add clamp to utils

--- src/utils.c
+++ src/utils.c
@@ -1,3 +1,14 @@
+#include <assert.h>
+
 int min(int a, int b) { return (a < b) ? a : b; }
+
 double absf(double a) { return a < 0 ? -a : a; };
 
+double clamp(double in, double from, double to) {
+  assert(from < to);
+  if (in > to)
+    return to;
+  if (in < from)
+    return from;
+  return in;
+}
--- src/utils.h
+++ src/utils.h
@@ -2,5 +2,5 @@
 #define UTILS_H
 int min(int a, int b);
 double absf(double a);
-
+double clamp(double in, double from, double to);
 #endif
